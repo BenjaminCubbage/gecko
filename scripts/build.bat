1b3c3001583af98b9efa5f7dbb3fb856ce23aee0
RecyclingBen <bdcubbage@gmail.com>
2025-11-19 16:52:45 -0500

    Flatten directory structure, run build_compression_wasm target when running build script

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,1 +1,2 @@
-cmd /c "cd /d ""%~dp0/.."" && cmake --build build --target gecko --config Release"
+cmd /c "cd /d ""%~dp0/.."" && cmake --build build --target gecko_compression_wasm --config Release"
+cmd /c "cd /d ""%~dp0/.."" && cmake --build build --target gecko_api              --config Release"
--- src/api/mosquitto-ctrl.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-for /f "usebackq tokens=1,2 delims==" %%A in ("%~dp0/scripts.env") do (
-    set "%%A=%%B"
-)
-
-"%MOSQUITTO_CTRL_PATH%" %*
