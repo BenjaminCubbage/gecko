8778c1f6a59aae5808dcfb8a1a7a366766a089d2
Benjamin Cubbage <bdcubbage@gmail.com>
2026-02-18 16:34:13 -0500

    Fix formatting inconsistency in compression's Bit.h

--- compression/src/gecko/Bit.h
+++ compression/src/gecko/Bit.h
@@ -3,24 +3,26 @@
 
 namespace Gecko::Compression
 {
-        class Bit
+
+class Bit
+{
+    public:
+        /*
+                Polyfills for std::byteswap
+        */
+
+        static uint32_t ByteSwap(uint32_t value)
         {
-          public:
-                /*
-                        Polyfills for std::byteswap
-                */
+                return ((value << 24) & 0xFF000000) |
+                        ((value << 8)  & 0x00FF0000) |
+                        ((value >> 8)  & 0x0000FF00) |
+                        ((value >> 24) & 0x000000FF);
+        }
 
-                static uint32_t ByteSwap(uint32_t value)
-                {
-                        return ((value << 24) & 0xFF000000) |
-                               ((value << 8)  & 0x00FF0000) |
-                               ((value >> 8)  & 0x0000FF00) |
-                               ((value >> 24) & 0x000000FF);
-                }
+        static uint16_t ByteSwap(uint16_t value)
+        {
+                return (value << 8) | (value >> 8);
+        }
+}; /* class Bit */
 
-                static uint16_t ByteSwap(uint16_t value)
-                {
-                        return (value << 8) | (value >> 8);
-                }
-        };
-}
+} /* namespace Gecko::Compression */
